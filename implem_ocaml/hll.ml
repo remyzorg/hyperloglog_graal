9ff293c1d04591f5f15f446cce2f5f66239f346d
remyzorg <besognet.remy@gmail.com>
2014-12-12 17:42:50 +0100

    leading 0

--- implem_ocaml/hll.ml
+++ implem_ocaml/hll.ml
@@ -10,14 +10,35 @@ let logn s n = Float.(
     (log n) / (log s)
   )
 
+let (<+) = (lsl)
+let (+>) = (lsr)
 
 let print_bin v  =
   for i = 31 downto 0 do
     Printf.printf "%d" (v lsr i land 1)
-  done
+  done;
+  print_endline ""
 
-let (<+) = (lsl)
-let (+>) = (lsr)
+let lsts_n n k = (k <+ n) +> n
+let fsts_n n k = (k <+ n) <+ n
+
+
+let (<+!) x n= x := !x <+ n
+
+
+let clz x =
+  let x = ref x in
+  let n = ref 0 in
+  if !x <= 0x0000ffff then (n <+! 16; x <+! 16);
+  if !x <= 0x00ffffff then (n <+! 8; x <+! 8);
+  if !x <= 0x0fffffff then (n <+! 4; x <+! 4);
+  if !x <= 0x3fffffff then (n <+! 2; x <+! 2);
+  if !x <= 0x7fffffff then incr n;
+  !n
+
+
+
+
 
 module type Data =
   sig
@@ -28,11 +49,13 @@ module type Data =
 module Make = functor (D: Data) -> struct
 
   let aggregation p set hash =
+    let htbl = Hashtbl.create 1777 in
     D.iter (fun v ->
         let x = hash v in
-        let idx = (x +> p) <+ p in
-        let w = (x <+ p) +> p in
-        let midx = max
+        let idx = lsts_n p x in
+        let w = fsts_n p x in
+        (* let midx = match Hashtbl.find *)
+        assert false
     ) set
 
 
